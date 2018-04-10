334adbe7a1989c038f7227624cd2ace9b2a0640b
Minh Hoang Tuan <minhhtml5@gmail.com>
2018-04-11 00:33:22 +0700

    - Some boss animation

--- Assets/GSGameplay/Background/SCR_Background.cs
+++ Assets/GSGameplay/Background/SCR_Background.cs
@@ -7,22 +7,24 @@ using UnityEngine;
 
 public class SCR_Background : MonoBehaviour {
 	// Const
-	private const int 	BACKGROUND_HEIGHT 			= 5760;
-	private const int 	BACKGROUND_LOOP_HEIGHT 		= 1920;
+	private const int 	BACKGROUND_HEIGHT 			= 1920;
+	private const int 	BACKGROUND_PART 			= 5;
 	
 	private const float BACKGROUND_SCROLL_RATIO		= 0.2f;
-	private const float MIDDLEGROUND_SCROLL_RATIO	= 0.5f;
+	private const float MIDDLEGROUND_SCROLL_RATIO	= 0.6f;
 	private const float FOREGROUND_SCROLL_RATIO		= 1.0f;
 	
+	private const float BACKGROUND_OFFSET			= 200;
+	private const float MIDDLEGROUND_OFFSET			= 0;
+
+	
 	// Prefab
-	public GameObject 		PFB_Background			= null;
-	public GameObject		PFB_BackgroundLoop		= null;
+	public GameObject[] 	PFB_Background			= null;
 	public GameObject		PFB_Middleground		= null;
 	public GameObject		PFB_Foreground			= null;
 	
 	// Object
-	private GameObject		background				= null;
-	private GameObject[] 	backgroundLoop			= null;
+	private GameObject[]	background				= null;
 	private GameObject 		middleground			= null;
 	private GameObject 		foreground				= null;
 	
@@ -35,15 +37,17 @@ public class SCR_Background : MonoBehaviour {
 	private void Start () {
 		instance = this;
 		
-		background = Instantiate (PFB_Background);
-		background.transform.localScale = new Vector3 (SCR_Gameplay.SCREEN_SCALE, SCR_Gameplay.SCREEN_SCALE, 1);
-		
-		backgroundLoop = new GameObject[2];
-		backgroundLoop[0] = Instantiate (PFB_BackgroundLoop);
-		backgroundLoop[1] = Instantiate (PFB_BackgroundLoop);
-		for (var i=0; i<2; i++) {
-			backgroundLoop[i].transform.localScale = new Vector3 (SCR_Gameplay.SCREEN_SCALE, SCR_Gameplay.SCREEN_SCALE, 1);
+		background = new GameObject[BACKGROUND_PART];
+		for (var i=0; i<BACKGROUND_PART; i++) {
+			background[i] = Instantiate (PFB_Background[i]);
+			background[i].transform.localScale = new Vector3 (SCR_Gameplay.SCREEN_SCALE, SCR_Gameplay.SCREEN_SCALE, 1);
 		}
+		
+		middleground = Instantiate (PFB_Middleground);
+		middleground.transform.localScale = new Vector3 (SCR_Gameplay.SCREEN_SCALE, SCR_Gameplay.SCREEN_SCALE, 1);
+		
+		foreground = Instantiate (PFB_Foreground);
+		foreground.transform.localScale = new Vector3 (SCR_Gameplay.SCREEN_SCALE, SCR_Gameplay.SCREEN_SCALE, 1);
 		
 		SetCameraY (0);
 	}
@@ -51,25 +55,42 @@ public class SCR_Background : MonoBehaviour {
 	// Set camera position
 	public static void SetCameraY (float cameraY) {
 		float backgroundY = cameraY * BACKGROUND_SCROLL_RATIO;
-		if (backgroundY < BACKGROUND_HEIGHT) {
-			instance.background.transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY, instance.background.transform.position.z);
-			instance.backgroundLoop[0].transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY + BACKGROUND_HEIGHT, instance.backgroundLoop[0].transform.position.z);
-			instance.backgroundLoop[1].SetActive (false);
+		
+		for (int i=0; i<BACKGROUND_PART; i++) {
+			instance.background[i].SetActive (false);
+		}
+		
+		if (backgroundY < (BACKGROUND_PART-1) * BACKGROUND_HEIGHT) {
+			for (int i=0; i<BACKGROUND_PART-1; i++) {
+				if (backgroundY >= i * BACKGROUND_HEIGHT && backgroundY < (i+1) * BACKGROUND_HEIGHT) {
+					instance.background[i].SetActive (true);
+					instance.background[i+1].SetActive (true);
+					
+					backgroundY = backgroundY % BACKGROUND_HEIGHT;
+					instance.background[i].transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY + BACKGROUND_OFFSET, instance.background[i].transform.position.z);
+					instance.background[i+1].transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY + BACKGROUND_HEIGHT + BACKGROUND_OFFSET, instance.background[i+1].transform.position.z);
+				}
+			}
 		}
 		else {
-			backgroundY = backgroundY - BACKGROUND_HEIGHT;
-			backgroundY = backgroundY % BACKGROUND_LOOP_HEIGHT;
-			for (var i=0; i<2; i++) {
-				instance.backgroundLoop[i].transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY + i * BACKGROUND_LOOP_HEIGHT, instance.backgroundLoop[i].transform.position.z);
-			}
-			instance.backgroundLoop[1].SetActive (true);
-			instance.background.SetActive (false);
+			backgroundY = backgroundY % BACKGROUND_HEIGHT;
+			instance.background[BACKGROUND_PART-2].SetActive (true);
+			instance.background[BACKGROUND_PART-1].SetActive (true);
+			instance.background[BACKGROUND_PART-2].transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY, instance.background[BACKGROUND_PART-2].transform.position.z);
+			instance.background[BACKGROUND_PART-1].transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -backgroundY + BACKGROUND_HEIGHT, instance.background[BACKGROUND_PART-1].transform.position.z);
 		}
+		
+		
+		float middlegroundY = cameraY * MIDDLEGROUND_SCROLL_RATIO;
+		instance.middleground.transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -middlegroundY + MIDDLEGROUND_OFFSET, instance.middleground.transform.position.z);
+		
+		float foregroundY = cameraY * FOREGROUND_SCROLL_RATIO;
+		instance.foreground.transform.position = new Vector3 (SCR_Gameplay.SCREEN_W * 0.5f, -foregroundY, instance.foreground.transform.position.z);
 	}
 	
 	private void Update () {
-		cameraTempY += Time.deltaTime * 3000;
-		SetCameraY (cameraTempY);
+		//cameraTempY += Time.deltaTime * 50;
+		//SetCameraY (cameraTempY);
 	}
 	
 }
---
+++ Assets/GSGameplay/Boss/SCR_Boss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BossState {
+	TALK = 0,
+	GRAB,
+	FLY,
+	SLIDE,
+	RUN
+}
+
+public class SCR_Boss : MonoBehaviour {
+	private Animator 	animator	= null;
+	private BossState 	state		= BossState.TALK;
+
+	private void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	private void SwitchAnimation () {
+		animator.SetInteger("AnimationClip", (int)state);
+	}
+	
+	private void Update () {
+		//GetComponent(SpriteRenderer).sprite = newSprite;
+	}
+}
